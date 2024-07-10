8612d48e4ef109d261f25178f07ba37801f3d8b9
usamanaseer <usama.naseer@invozone.com>
2024-07-11 02:30:03 +0500

    strapi

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
-
 
     postgres: {
       connection: {
@@ -14,8 +13,16 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          ca: env('DATABASE_CA')
+        ssl: env.bool('DATABASE_SSL', false) && {
+          key: env('DATABASE_SSL_KEY', undefined),
+          cert: env('DATABASE_SSL_CERT', undefined),
+          ca: env('DATABASE_SSL_CA', undefined),
+          capath: env('DATABASE_SSL_CAPATH', undefined),
+          cipher: env('DATABASE_SSL_CIPHER', undefined),
+          rejectUnauthorized: env.bool(
+            'DATABASE_SSL_REJECT_UNAUTHORIZED',
+            true
+          ),
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
@@ -27,7 +34,7 @@ module.exports = ({ env }) => {
   return {
     connection: {
       client,
-      ...connections[client],
+      ...connections['postgres'],
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
   };
